dc70aa6731d2a419b0ec7fdb7f65a3bea65eae29
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-11-21 11:09:46 +0000

    config: update urls to production render link

--- mobile/src/graphql/apollo.ts
+++ mobile/src/graphql/apollo.ts
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const httpLink = createHttpLink({
-  uri: 'https://nonmanipulatory-fearsomely-nathanial.ngrok-free.dev/graphql/', 
+  uri: 'https://gemconnect-backend.onrender.com/graphql/', 
 });
 
 const authLink = setContext(async (_, { headers }) => {
